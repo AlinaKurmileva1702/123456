ef539f8bb561e07bd9e00da61b6e45b539431093
AlinaKurmileva1702 <56438388+AlinaKurmileva1702@users.noreply.github.com>
2019-10-12 08:40:31 +0700

    Update алина.py

--- алина.py
+++ алина.py
@@ -2,7 +2,7 @@ from tkinter import*
 
 root=Tk()
 root.title("Численность популяций")
-root.geometry("450x400")
+root.geometry("500x500")
 lb_neogr=Label(root,text="Неограниченный рост",width=51,bg="red")
 lb_neogr.place(x=45,y=20)
 
@@ -11,6 +11,9 @@ lb_a.place(x=10,y=50)
 
 ent_a=Entry(root,bg="white",width=10)
 ent_a.place(x=45,y=50)
+
+bt_1=Button(root,text="Неограниченный рост",width=10)
+bt_1.place(x=145,y=50)
 
 lb_Xn1=Label(root,text="Xn",bg="orange",width=2)
 lb_Xn1.place(x=250,y=50)
@@ -27,6 +30,9 @@ lb_b.place(x=10,y=110)
 ent_b=Entry(root,bg="white",width=10)
 ent_b.place(x=45,y=110)
 
+bt_2=Button(root,text="Ограниченный рост",width=10)
+bt_2.place(x=145,y=110)
+
 lb_Xn2=Label(root,text="Xn",bg="green",width=2)
 lb_Xn2.place(x=250,y=110)
 
@@ -41,6 +47,9 @@ lb_a.place(x=10,y=170)
 
 ent_c=Entry(root,bg="white",width=10)
 ent_c.place(x=45,y=170)
+
+bt_3=Button(root,text="Ограниченный рост с отловом",width=10)
+bt_3.place(x=145,y=170)
 
 lb_Xn3=Label(root,text="Xn",bg="purple",width=2)
 lb_Xn3.place(x=250,y=170)
@@ -57,6 +66,9 @@ lb_d.place(x=10,y=230)
 ent_d=Entry(root,bg="white",width=5)
 ent_d.place(x=35,y=230)
 
+bt_4=Button(root,text="Жертва-хищник",width=10)
+bt_4.place(x=210,y=230)
+
 lb_f=Label(root,text="f",bg="orange",width=2)
 lb_f.place(x=75,y=230)
 
@@ -70,16 +82,16 @@ ent_q=Entry(root,bg="white",width=5)
 ent_q.place(x=165,y=230)
 
 lb_Xn4=Label(root,text="Жер.",bg="green",width=3)
-lb_Xn4.place(x=220,y=230)
+lb_Xn4.place(x=220,y=270)
 
 lb_Xn4_otv=Label(root,bg="white",width=8)
-lb_Xn4_otv.place(x=245,y=230)
+lb_Xn4_otv.place(x=245,y=270)
 
 lb_Yn=Label(root,text="Хищ.",bg="blue",width=3)
-lb_Yn.place(x=320,y=230)
+lb_Yn.place(x=320,y=270)
 
 lb_Yn_otv=Label(root,bg="white",width=8)
-lb_Yn_otv.place(x=345,y=230)
+lb_Yn_otv.place(x=345,y=270)
 
 lb_x1=Label(root,text="Жер.",bg="orange",width=5)
 lb_x1.place(x=10,y=260)
@@ -100,7 +112,7 @@ ent_n=Entry(root,bg="white",width=10)
 ent_n.place(x=55,y=320)
 
 canv=Canvas(root,width=260,height=130,bg="white")
-canv.place(x=140,y=260)
+canv.place(x=140,y=300)
 
 bt=Button(root,text="График",width=10)
 bt.place(x=12,y=350)
